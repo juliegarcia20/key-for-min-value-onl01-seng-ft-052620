74a1d3a0783d918f2cc48bf0074b463d48f7c326
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-03 16:44:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,3 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+def key_for_min_value(hash)
+  values = hash.values
+  hash.keys[values.index(values.min).to_i]
+end
